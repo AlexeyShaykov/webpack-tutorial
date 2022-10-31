2b969764c8a871486afbba143e898be9d8eff241
Shaykov Alexey <kayfolom@inbox.ru>
2022-10-31 11:03:05 +0300

    add react

--- src/index.js
+++ src/index.js
@@ -1,44 +1,10 @@
-import dogs from 'asserts/dogs';
+import React from 'react';
+import ReactDom from 'react-dom/client';
 
-import './index.scss';
+import App from './App';
 
-console.log('hello world');
+const divRoot = document.getElementById('root');
 
-class MyDog {
-  name = 'Lucky'
-};
+const root = ReactDom.createRoot(divRoot);
 
-const myDog = new MyDog();
-
-const p = document.createElement('p');
-p.textContent = `My dog name is ${myDog.name}`;
-
-const h = document.createElement('h1');
-h.textContent = 'Hello world!!!';
-
-const ul = document.createElement('ul');
-
-ul.className = 'dogs-container';
-
-dogs.forEach(({ name, image }) => {
-  const li = document.createElement('li');
-  const div = document.createElement('div');
-  const img = document.createElement('img');
-
-  li.className = 'dogs-container-item';
-
-  img.src = image;
-  img.className = 'dogs-container-item__image';
-
-  div.textContent = name;
-  div.className = 'dogs-container-item__name';
-
-  li.appendChild(div);
-  li.appendChild(img);
-
-  ul.appendChild(li);
-});
-
-const app = document.querySelector('#root');
-
-app.append(h, p, ul);
+root.render(<App />);
